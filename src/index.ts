b973175d126160f9b20c0f4ee2ba5bab1fd24319
Kunal Dangi <kunaldangi252@gmail.com>
2025-06-09 14:55:17 +0530

    feat: added session verfication on secured routes and endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { config } from 'dotenv'; config();
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
 
 import initializeDatabase from './config/db';
 import initializeRoutes from './api/index';
@@ -9,13 +11,32 @@ import initializeRoutes from './api/index';
 const app = express();
 initializeDatabase();
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 initializeRoutes(app);
 
-app.use(express.static('public'));
 app.use((req, res, next) => {
-    console.log(`Request Path: ${req.path}`);
+    if(req.path == '/'){
+        let session = req.cookies.session;
+        if(!session) return res.redirect('/login');
+    
+        try {
+            session = jwt.verify(session, `${process.env.JWT_SECRET}`);
+        } catch (err) {
+            console.error('Invalid session token:', err);
+            return res.redirect('/login');
+        }
+    
+        console.log('Session: ', session);
+    }
+    else {
+        console.log('Skipping session check for login/register');
+        next();
+    }
+}, express.static('public'));
+
+app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
